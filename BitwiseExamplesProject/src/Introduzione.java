7ca4385f6a58cd57a7db5d259fab5d2d40d0db90
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2024-01-25 13:50:45 +0100

    Update Introduzione.java

--- BitwiseExamplesProject/src/Introduzione.java
+++ BitwiseExamplesProject/src/Introduzione.java
@@ -1,41 +1,43 @@
 public class Introduzione {
     public static void main(String[] args) {
-        // BITWISE
-        // 8bit per rappresentare i numeri
-        // tutti i risultati vanno in variabili
+        //BITWISE sono operatori che eseguono operatzioni
+        // sulle rappresentazioni binarie delle variabili
 
         int v1 = 10;
         int v2 = 0b00110010;
-        // Integer.toBinaryString(v)
-        /*
-         * Mettere tra parentesi tonde un valore intero (diretto o varaibile)
-         * Risultato => stringa binaria corrsipondente al valore immesso
-         * */
-        System.out.println(Integer.toBinaryString(v1));
-        System.out.println(Integer.toBinaryString(50));
-        System.out.println("-50> " + Integer.toBinaryString(-50));
-        // TILDE = ALT+126 (del tastierino numerico)
-        System.out.println("-50> " + ~50);
+        //Integer.toBinaryString(v1) stampa la stringa binaria del valore intero passato tra ()
+        System.out.println(v1 + " in binario è: " + Integer.toBinaryString(v1));
         System.out.println(v2);
-
+        System.out.println(Integer.toBinaryString(50));
 
         // &
         int valore = 123;
         int valore2 = 7;
-        System.out.println("valore=" + valore);
-        System.out.println("valore in binario:> " + Integer.toBinaryString(valore));
-        System.out.println("valore2=" + valore2);
-        System.out.println("valore2 in binario:> " + Integer.toBinaryString(valore2));
+        System.out.println(valore + ":> " + Integer.toBinaryString(valore));
+        System.out.println(valore2 + ":> " + Integer.toBinaryString(valore2));
 
-        // Mascheramento / masking
-        System.out.println("valore & valore= " + (valore & valore));
-        System.out.println("valore & 0= " + (valore & 0));
-        System.out.println("valore & valore2= " + (valore & valore2));
+        // ogni operatore bitwise restituisce un valore intero
+        // valore = 1111011 &
+        // valore = 1111011 =
+        // ------------------
+        //          1111011
+        System.out.println("valore & valore =" + (valore & valore));
+        System.out.println("valore & 0 =" + (valore & 0));
+        // valore = 01111011 &
+        // valore = 00000011 = (maschera)
+        // ------------------
+        //         000000011
+        System.out.println("valore & 1 = " + (valore & 1));
+        System.out.println("valore & 00000010 :> " + (valore & 0b00000010));
+        System.out.println("valore & 00000011:> " + (valore & 0b00000011));
 
-        System.out.println("valore & 1= " + (valore & 1));
-
-        System.out.println("valore & 0b00010000= " + (valore & 0b00010000));
-        System.out.println("valore & 0b00000100= " + (valore & 0b00000100));
-
+        // << = shift a sx (moltiplicazione per una potenza di due)
+        // >> = shift a dx (divisione per una potenza di due)
+        int v = 4; //0b00000100
+        System.out.println("v << 1 = " + (v << 1));
+        System.out.println("v << 2 = " + (v << 2));
+        System.out.println("v << 4 = " + (v << 4));
+        System.out.println();
+        System.out.println("v >> 1 = " + (v >> 1));
     }
 }
