9c0eda1c8dc31f9701730b2fd77d02945031b674
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2024-02-22 12:08:09 +0100

    Update SetBitsWithOR.java

--- BitwiseExamplesProject/src/SetBitsWithOR.java
+++ BitwiseExamplesProject/src/SetBitsWithOR.java
@@ -8,16 +8,16 @@ public class SetBitsWithOR {
         //Solo un semplice controllo per evitare di sforare.
         if (n < 0 || n > 32) {
             System.out.println("Il valore di n deve essere compreso tra 0 e 32.");
-            return;
+        } else {
+
+            //Calcola la maschera per impostare a 1 tutti i bit tranne i primi n bit
+            int maschera = (1 << n) - 1;
+
+            int risultato = numero | maschera;
+
+            System.out.println("Numero di partenza: " + Integer.toBinaryString(numero));
+            System.out.println("Maschera: " + Integer.toBinaryString(maschera));
+            System.out.println("Risultato: " + Integer.toBinaryString(risultato));
         }
-
-        //Calcola la maschera per impostare a 1 tutti i bit tranne i primi n bit
-        int maschera = (1 << n) - 1;
-
-        int risultato = numero | maschera;
-
-        System.out.println("Numero di partenza: "+ Integer.toBinaryString(numero));
-        System.out.println("Maschera: " + Integer.toBinaryString(maschera));
-        System.out.println("Risultato: " + Integer.toBinaryString(risultato));
     }
 }
